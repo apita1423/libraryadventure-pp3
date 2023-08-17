ea0fd547b82c06042ff0d1e24ba395014f4fc8dc
apita1423 <>
2023-08-17 12:04:22 +0000

    Add import of time, sys, and colorama and code block for typewriter effect

---
+++ run.py
@@ -0,0 +1,15 @@
+# import modules
+import time
+import sys
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
+
+
+# typewriter effect
+# code taken from Learn Learn Scratch Tutorials - YouTube link in README
+def typewriter(effect):
+    for char in effect:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.1)
