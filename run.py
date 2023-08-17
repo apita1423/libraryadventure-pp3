3406371a0c9bbb56f21313703b87f3847dcfebc6
apita1423 <>
2023-08-17 15:44:54 +0000

    Fix some spelling mistakes in story_begins

--- run.py
+++ run.py
@@ -36,8 +36,8 @@ print(r"""
  /_/    \_\__,_| \_/ \___|_| |_|\__|\__,_|_|  \___|
  """)
 
-print(r"""
-       .--.                   .---.
+print(fr"""{Fore.CYAN}
+    .--.                   .---.
 
    .---|__|           .-.     |~~~|
 
@@ -57,15 +57,19 @@ print(r"""
 
 ^--^---'--^    `-'`---^-^--^--^---'--' hjw
 """)
+print()
 
 
 # the library needs to know your name
-name = input("Please enter your name: ").capitalize().strip()
+name = input(f"{Fore.YELLOW}Please enter your name: ").capitalize().strip()
 print(f"Welcome to the library {name}! Hope you find something special.")
+time.sleep(2)
+print()
 
 
 # the story begins
 def story_begins():
+    typewriter("Let the adventure begin... \n")
     typewriter("As you walk through the aisle of books, a red book catches \n")
     typewriter("your eye. You pick it up and skim through, not realising \n")
     typewriter("that it's a magical book, and that it has picked you for a \n")
@@ -73,8 +77,8 @@ def story_begins():
     typewriter("As you read the book, the library starts to disappear. \n")
     typewriter("You find yourself in a magical land and face-to-face with \n")
     typewriter("a wizard. The wizard has been hurt in a battle to bring \n")
-    typewriter("magic back to land. The wizard senses your powers growing \n")
-    typewriter("gives you his powers.")
+    typewriter("magic back to the land. The wizard senses your powers \n")
+    typewriter("growing and gives you his powers.")
 
 
 story_begins()
